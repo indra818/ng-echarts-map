3886e6bdb95cf44e2a18f72ec1cd0e95cb1b2c31
Kyeongjin Kim <indra818@gmail.com>
2018-11-07 09:51:52 +0900

    import ECharts module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    NgxEchartsModule,
     AppRoutingModule
   ],
   providers: [],
